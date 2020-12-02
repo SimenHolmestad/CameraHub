fb2c6d9b208a485678393d54ed3f9070f22bad95
Simen Holmestad <simehol@stud.ntnu.no>
2020-12-02 20:37:37 +0100

    Redirect to album page after creating album

--- frontend/src/components/NewAlbumForm.js
+++ frontend/src/components/NewAlbumForm.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { create_or_update_album } from './../server'
+import { Redirect } from "react-router-dom";
 
 function NewAlbumForm(props) {
   const [albumName, setAlbumName] = React.useState("new_album");
   const [description, setDescription] = React.useState("description");
+  const [redirectAlbum, setRedirectAlbum] = React.useState(null);
 
   const handleSubmit = async (e) => {
   e.preventDefault();
-    await create_or_update_album(albumName, description)
+    const response = await create_or_update_album(albumName, description)
+    setRedirectAlbum(response.album_name)
   };
+
+  if (redirectAlbum) {
+    return <Redirect to={"/album/" + redirectAlbum}/>
+  }
 
   return (
     <form className="input-form" onSubmit={handleSubmit}>
