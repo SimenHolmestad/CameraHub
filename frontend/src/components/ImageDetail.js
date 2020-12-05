2b31752da0217b350f760c302d2d4b010715078b
Simen Holmestad <simehol@stud.ntnu.no>
2020-12-05 11:03:29 +0100

    Show loading icon when changing to next or previous image
    I tried to replace the image with a loading icon instead, but that
    caused the image to blink for every image switch. Having a loading
    icon for the button which is pressed seem to work better.
    
    It is important to have a loading icon as the images might be quite
    big.

--- frontend/src/components/ImageDetail.js
+++ frontend/src/components/ImageDetail.js
@@ -5,6 +5,7 @@ import ArrowForward from '@material-ui/icons/ArrowForward';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
   leftIcon: {
@@ -28,21 +29,54 @@ const useStyles = makeStyles((theme) => ({
 
 function ImageDetail({imageUrls, imageIndex, setImageIndex}) {
   const classes = useStyles();
+  const [leftIsLoading, setLeftIsLoading] = React.useState(false);
+  const [rightIsLoading, setRightIsLoading] = React.useState(false);
 
   const goToNextImage = () => {
     if (imageIndex > 1) {
       setImageIndex(imageIndex - 1)
+      setRightIsLoading(true)
     }
   }
 
   const goToPreviousImage = () => {
     if (imageIndex < imageUrls.length) {
       setImageIndex(imageIndex + 1)
+      setLeftIsLoading(true)
     }
+  }
+
+  const doneLoading = () => {
+    setLeftIsLoading(false)
+    setRightIsLoading(false)
   }
 
   const leftButtonDisabled = imageIndex >= imageUrls.length
   const rightButtonDisabled = imageIndex <= 1
+
+  let leftButton = null
+  if (leftIsLoading) {
+    leftButton = <CircularProgress size="2em"/>
+  } else {
+    leftButton = (
+      <Button onClick={goToPreviousImage} disabled={leftButtonDisabled}>
+        <ArrowBack className={classes.leftIcon} />
+        Previous image
+      </Button>
+    )
+  }
+
+  let rightButton = null
+  if (rightIsLoading) {
+    rightButton = <CircularProgress size="2em"/>
+  } else {
+    rightButton = (
+      <Button onClick={goToNextImage} disabled={rightButtonDisabled}>
+        Next image
+        <ArrowForward className={classes.rightIcon} />
+      </Button>
+    )
+  }
 
   return (
     <>
@@ -54,17 +88,12 @@ function ImageDetail({imageUrls, imageIndex, setImageIndex}) {
       </Grid>
       <Container className={classes.imageContainer}>
         <img className={classes.image}
+             onLoad={doneLoading}
              src={imageUrls[imageUrls.length - imageIndex]}
              alt=""/>
         <Grid container justify="space-between">
-          <Button onClick={goToPreviousImage} disabled={leftButtonDisabled}>
-            <ArrowBack className={classes.leftIcon} />
-            Previous image
-          </Button>
-          <Button onClick={goToNextImage} disabled={rightButtonDisabled}>
-            Next image
-            <ArrowForward className={classes.rightIcon} />
-          </Button>
+          {leftButton}
+          {rightButton}
         </Grid>
       </Container>
     </>
