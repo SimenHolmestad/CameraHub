3acb48eb414df5cd0c6bf6d5c3f18e10449ea856
Simen Holmestad <simehol@stud.ntnu.no>
2020-12-06 12:11:02 +0100

    Change the footer message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,7 +57,7 @@ function App() {
         {/* Footer */}
         <footer className={classes.footer}>
           <Typography variant="h6" align="center" gutterBottom>
-            Why are you reading this? Go take som pictures!
+            Hope you like CameraHub!
           </Typography>
           <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
             CameraHub is made using Python, Flask, React and Material UI. The source code is openly available on <a href="https://github.com/SimenHolmestad/CameraHub" target="_blank" rel="noreferrer">GitHub</a>.
