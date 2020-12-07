38f7a6ec20224328cb754f57931146922ee083aa
Simen Holmestad <simehol@stud.ntnu.no>
2020-12-07 13:44:15 +0100

    Center last_image correctly and create better empty album message
    The css-centering code was mainly found here:
    https://stackoverflow.com/questions/18490334/responsive-image-full-screen-and-centered-maintain-aspect-ratio-not-exceed-wi/18490463

--- frontend/src/components/LastImage.js
+++ frontend/src/components/LastImage.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { get_last_image_url } from './../server'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles(() => ({
   image: {
-    width: "100%",
-    height: "100vh",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    bottom: "0",
+    left: "0",
+    margin: "auto",
+    position: "fixed",
+    right: "0",
+    top: "0",
+    objectFit: "contain",
   },
   background: {
     position: "fixed",
@@ -15,6 +24,13 @@ const useStyles = makeStyles(() => ({
     width: "100%",
     height:" 100%",
     backgroundColor: "black"
+  },
+  noImagesDiv: {
+    height: "90vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
   }
 }));
 
@@ -23,33 +39,44 @@ function LastImage(props) {
   const [albumEmpty, setAlbumEmpty] = React.useState(false);
   const [albumExists, setAlbumExists] = React.useState(true);
   const classes = useStyles();
-
-  const getLastImageData = () => {
-    get_last_image_url(albumName).then((data) => {
-      if (data["error"]) {
-        if (data["error"] === "album is empty") {
-          setAlbumEmpty(true)
-        } else {
-          setAlbumExists(false)
-        }
-      }
-      setImageUrl(data["last_image_url"]);
-    });
-  }
-
   const albumName = props.match.params.albumName;
+
   React.useEffect(() => {
+    const getLastImageData = () => {
+      get_last_image_url(albumName).then((data) => {
+        if (data["error"] && data["error"] === "album is empty") {
+          setAlbumEmpty(true)
+        } else if (data["error"]) {
+          setAlbumExists(false)
+        } else {
+          setImageUrl(data["last_image_url"]);
+          setAlbumEmpty(false)
+        }
+      });
+    }
+
     getLastImageData()
-    const interval = setInterval(getLastImageData, 1000);
+    const interval = setInterval(getLastImageData, 1500);
     return () => clearInterval(interval);
   }, [albumName]);
 
   if (!albumExists) {
-    return <p>There is no album named {albumName}</p>
+    return <h1>There is no album named {albumName}</h1>
   }
 
   if (albumEmpty) {
-    return <p>Album is empty</p>
+    return (
+      <div className={ classes.noImagesDiv }>
+        <Container justify="center" maxWidth="sm">
+          <Typography variant="h3" className={classes.emptyAlbumText} align="center" color="textSecondary" gutterBottom>
+            There are no images
+          </Typography>
+          <Typography variant="h5" className={classes.emptyAlbumText} align="center" color="textSecondary" paragraph>
+            There are currently no images in this album. The last image captured will appear here when images are captured to the album.
+          </Typography>
+        </Container>
+      </div>
+    )
   }
 
   if (!imageUrl) {
