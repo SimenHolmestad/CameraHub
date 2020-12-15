3b9cd9e61594efbad4f1e5474ae2d073e1ef2656
Simen Holmestad <simehol@stud.ntnu.no>
2020-12-15 10:41:09 +0100

    Create folder class to avoid repetitive code folder_album

---
+++ backend/album_storage/folder.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+from pathlib import Path
+
+
+class Folder():
+    """A class for interacting with a folder"""
+
+    def __init__(self, base_dir, dir_name):
+        self.base_dir = base_dir
+        self.dir_name = dir_name
+        self.create_folder_if_not_exist()
+
+    def get_folder_contents(self):
+        path_to_folder = self.get_path()
+        return os.listdir(path_to_folder)
+
+    def get_sorted_folder_contents(self):
+        return sorted(self.get_folder_contents())
+
+    def get_path(self):
+        return os.path.join(
+            self.base_dir,
+            self.dir_name
+        )
+
+    def count_files(self):
+        """Return number of files inside the folder"""
+        return len(self.get_folder_contents())
+
+    def get_path_to_file(self, filename):
+        return os.path.join(
+            self.get_path(),
+            filename
+        )
+
+    def get_relative_url_to_file(self, filename):
+        path_to_files = self.get_path_to_file(filename)
+        path = list(Path(path_to_files).parts)
+        return "/".join(path)
+
+    def get_relative_urls_to_all_files(self):
+        file_names = self.get_sorted_folder_contents()
+        urls = list(map(
+            lambda name: self.get_relative_url_to_file(name),
+            file_names
+        ))
+        return urls
+
+    def write_file_in_folder(self, filename, content):
+        path_to_file = self.get_path_to_file(filename)
+        with open(path_to_file, "w") as f:
+            f.write(content)
+
+    def read_file_in_folder(self, filename):
+        path_to_file = self.get_path_to_file(filename)
+        with open(path_to_file, "r") as f:
+            return f.read()
+
+    def file_exists_in_folder(self, filename):
+        path_to_file = self.get_path_to_file(filename)
+        return os.path.exists(path_to_file)
+
+    def remove_all_folder_content(self):
+        path_to_folder = self.get_path()
+        shutil.rmtree(path_to_folder)
+        os.mkdir(path_to_folder)
+
+    def create_folder_if_not_exist(self):
+        if not os.path.exists(self.get_path()):
+            os.makedirs(self.get_path())
--- backend/album_storage/folder_album.py
+++ backend/album_storage/folder_album.py
@@ -1,7 +1,6 @@
-import os
-import shutil
 from PIL import Image
 from .base_album import BaseAlbum
+from .folder import Folder
 MAX_THUMBNAIL_SIZE = (600, 600)
 
 
@@ -11,21 +10,20 @@ class FolderAlbum(BaseAlbum):
     """
 
     def __init__(self, album_name, path_to_album_folders, image_name_prefix="image"):
-        self.path_to_album_folders = path_to_album_folders
-        self.album_name = album_name
-        self.image_name_prefix = image_name_prefix
-        self.path_to_album_folders_name = os.path.split(path_to_album_folders)[1]
+        self.album_folder = Folder(path_to_album_folders, album_name)
+        self.images_folder = Folder(self.album_folder.get_path(), "images")
+        self.thumbnails_folder = Folder(self.album_folder.get_path(), "thumbnails")
 
-        self.__ensure_correct_folder_structure()
+        self.image_name_prefix = image_name_prefix
 
     def get_album_description(self):
-        if not self.__description_file_exists():
+        if not self.album_folder.file_exists_in_folder("description.txt"):
             return ""
 
-        return self.__get_description_file_content()
+        return self.album_folder.read_file_in_folder("description.txt")
 
     def set_album_description(self, content):
-        self.__write_description_file(content)
+        self.album_folder.write_file_in_folder("description.txt", content)
 
     def get_relative_url_of_last_image(self):
         """Returns the url of the last image captured to the album."""
@@ -35,27 +33,17 @@ class FolderAlbum(BaseAlbum):
 
         if self.__image_with_number_and_extension_exists(current_image_number, ".jpg"):
             image_filename = self.__format_image_name(current_image_number, ".jpg")
-            return self.__create_relative_url_to_image_file(image_filename)
+            return self.images_folder.get_relative_url_to_file(image_filename)
 
         if self.__image_with_number_and_extension_exists(current_image_number, ".png"):
             image_filename = self.__format_image_name(current_image_number, ".png")
-            return self.__create_relative_url_to_image_file(image_filename)
+            return self.images_folder.get_relative_url_to_file(image_filename)
 
     def get_relative_urls_of_all_images(self):
-        image_filenames = self.__get_sorted_list_of_image_names()
-        urls = list(map(
-            lambda name: self.__create_relative_url_to_image_file(name),
-            image_filenames
-        ))
-        return urls
+        return self.images_folder.get_relative_urls_to_all_files()
 
     def get_relative_urls_of_all_thumbnails(self):
-        image_filenames = self.__get_sorted_list_of_thumbnail_names()
-        urls = list(map(
-            lambda name: self.__create_relative_url_to_thumbnail_file(name),
-            image_filenames
-        ))
-        return urls
+        return self.thumbnails_folder.get_relative_urls_to_all_files()
 
     def try_capture_image_to_album(self, camera_module):
         next_image_number = self.__get_current_image_number() + 1
@@ -64,106 +52,41 @@ class FolderAlbum(BaseAlbum):
             camera_module.file_extension
         )
 
-        next_image_filepath = self.__get_image_path_from_name(next_image_name)
+        next_image_filepath = self.images_folder.get_path_to_file(next_image_name)
         camera_module.try_capture_image(next_image_filepath)
 
         self.__create_thumbnail_for_image(next_image_name)
         self.__set_current_image_number(next_image_number)
 
     def ensure_thumbnails_correct(self):
-        self.__create_thumbnails_folder_if_not_exists()
-        if not self.__count_images() == self.__count_thumbnails():
+        self.thumbnails_folder.create_folder_if_not_exist()
+
+        number_of_images = self.images_folder.count_files()
+        number_of_thumbnails = self.thumbnails_folder.count_files()
+        if not number_of_images == number_of_thumbnails:
             self.recreate_all_thumbnails()
 
     def recreate_all_thumbnails(self):
-        self.__delete_all_thumbnails()
+        self.thumbnails_folder.remove_all_folder_content()
 
-        image_names = self.__get_all_image_names()
+        image_names = self.images_folder.get_folder_contents()
         for name in image_names:
             self.__create_thumbnail_for_image(name)
 
-    # Private methods below
-
-    def __get_path_to_album_folder(self):
-        return os.path.join(
-            self.path_to_album_folders,
-            self.album_name
-        )
-
-    def __get_path_to_images_folder(self):
-        return os.path.join(
-            self.__get_path_to_album_folder(),
-            "images"
-        )
-
-    def __get_path_to_thumbnails_folder(self):
-        return os.path.join(
-            self.__get_path_to_album_folder(),
-            "thumbnails"
-        )
-
-    def __get_path_to_description_file(self):
-        return os.path.join(
-            self.__get_path_to_album_folder(),
-            "description.txt"
-        )
-
-    def __get_path_to_current_image_number_file(self):
-        return os.path.join(
-            self.__get_path_to_album_folder(),
-            ".current_image_number.txt"
-        )
-
     def __current_image_number_file_exists(self):
-        return os.path.exists(self.__get_path_to_current_image_number_file())
-
-    def __get_current_image_number_file_content(self):
-        with open(self.__get_path_to_current_image_number_file()) as f:
-            return int(f.read())
-
-    def __write_current_image_number_file(self, image_number):
-        with open(self.__get_path_to_current_image_number_file(), "w") as f:
-            f.write(str(image_number))
+        return self.album_folder.file_exists_in_folder(".image_number.txt")
 
     def __set_current_image_number(self, image_number):
-        self.__write_current_image_number_file(image_number)
-
-    def __description_file_exists(self):
-        return os.path.exists(self.__get_path_to_description_file())
-
-    def __get_description_file_content(self):
-        with open(self.__get_path_to_description_file()) as f:
-            return f.read()
-
-    def __write_description_file(self, content):
-        with open(self.__get_path_to_description_file(), "w") as f:
-            f.write(content)
-
-    def __create_album_folder_if_not_exists(self):
-        if not os.path.exists(self.__get_path_to_album_folder()):
-            os.makedirs(self.__get_path_to_album_folder())
-
-    def __create_images_folder_if_not_exists(self):
-        images_folder_path = self.__get_path_to_images_folder()
-        if not os.path.exists(images_folder_path):
-            os.makedirs(images_folder_path)
-
-    def __create_thumbnails_folder_if_not_exists(self):
-        if not os.path.exists(self.__get_path_to_thumbnails_folder()):
-            os.makedirs(self.__get_path_to_thumbnails_folder())
-
-    def __ensure_correct_folder_structure(self):
-        self.__create_album_folder_if_not_exists()
-        self.__create_images_folder_if_not_exists()
-        self.__create_thumbnails_folder_if_not_exists()
+        self.album_folder.write_file_in_folder(".image_number.txt", str(image_number))
 
     def __ensure_current_image_number_file_exists(self):
         if not self.__current_image_number_file_exists():
-            self.__write_current_image_number_file(0)
+            self.__set_current_image_number(0)
 
     def __image_with_number_and_extension_exists(self, image_number, extension):
-        file_path = self.__create_image_filepath(image_number, extension)
-        if os.path.exists(file_path):
+        possible_file_name = self.__format_image_name(image_number, extension)
+
+        if self.images_folder.file_exists_in_folder(possible_file_name):
             return True
 
     def __image_with_image_number_exists(self, image_number):
@@ -176,7 +99,7 @@ class FolderAlbum(BaseAlbum):
         return False
 
     def __current_image_number_correct(self):
-        current_image_number = self.__get_current_image_number_file_content()
+        current_image_number = int(self.album_folder.read_file_in_folder(".image_number.txt"))
         if not self.__image_with_image_number_exists(current_image_number):
             return False
 
@@ -192,14 +115,14 @@ class FolderAlbum(BaseAlbum):
 
     def __recreate_current_image_number_file(self):
         """Recreates the image number file based on the image files present"""
-        image_names = self.__get_sorted_list_of_image_names()
+        image_names = self.images_folder.get_sorted_folder_contents()
         if not image_names:
             image_number = 0
         else:
             last_name = image_names[-1]
             image_number = self.__get_image_number_from_filename(last_name)
 
-        self.__write_current_image_number_file(image_number)
+        self.__set_current_image_number(image_number)
 
     def __ensure_current_image_number_file_correct(self):
         if not self.__current_image_number_correct():
@@ -208,48 +131,10 @@ class FolderAlbum(BaseAlbum):
     def __get_current_image_number(self):
         self.__ensure_current_image_number_file_exists()
         self.__ensure_current_image_number_file_correct()
-        return self.__get_current_image_number_file_content()
+        return int(self.album_folder.read_file_in_folder(".image_number.txt"))
 
     def __format_image_name(self, image_number, file_extension):
         return self.image_name_prefix + str(image_number).rjust(4, "0") + file_extension
-
-    def __get_image_path_from_name(self, image_name):
-        return os.path.join(
-            self.__get_path_to_images_folder(),
-            image_name
-        )
-
-    def __create_image_filepath(self, image_number, file_extension):
-        return os.path.join(
-            self.__get_path_to_images_folder(),
-            self.__format_image_name(image_number, file_extension)
-        )
-
-    def __create_thumbnail_filepath(self, image_number):
-        return os.path.join(
-            self.__get_path_to_thumbnails_folder(),
-            self.__format_image_name(image_number, ".jpg")
-        )
-
-    def __get_sorted_list_of_image_names(self):
-        return sorted(self.__get_all_image_names())
-
-    def __get_sorted_list_of_thumbnail_names(self):
-        return sorted(self.__get_all_thumbnail_names())
-
-    def __create_relative_url_to_image_file(self, image_filename):
-        return "{}/{}/images/{}".format(
-            self.path_to_album_folders_name,
-            self.album_name,
-            image_filename
-        )
-
-    def __create_relative_url_to_thumbnail_file(self, thumbnail_filename):
-        return "{}/{}/thumbnails/{}".format(
-            self.path_to_album_folders_name,
-            self.album_name,
-            thumbnail_filename
-        )
 
     def __create_thumbnail(self, input_path, output_path):
         image = Image.open(input_path)
@@ -259,27 +144,10 @@ class FolderAlbum(BaseAlbum):
 
     def __get_thumbnail_path_from_image_name(self, image_name):
         image_number = self.__get_image_number_from_filename(image_name)
-        return self.__create_thumbnail_filepath(image_number)
+        thumbnail_file_name = self.__format_image_name(image_number, ".jpg")
+        return self.thumbnails_folder.get_path_to_file(thumbnail_file_name)
 
     def __create_thumbnail_for_image(self, image_name):
-        image_path = self.__get_image_path_from_name(image_name)
+        image_path = self.images_folder.get_path_to_file(image_name)
         thumbnail_path = self.__get_thumbnail_path_from_image_name(image_name)
         self.__create_thumbnail(image_path, thumbnail_path)
-
-    def __delete_all_thumbnails(self):
-        shutil.rmtree(self.__get_path_to_thumbnails_folder())
-        self.__create_thumbnails_folder_if_not_exists()
-
-    def __get_all_image_names(self):
-        images_folder = self.__get_path_to_images_folder()
-        return os.listdir(images_folder)
-
-    def __get_all_thumbnail_names(self):
-        thumbnails_folder = self.__get_path_to_thumbnails_folder()
-        return os.listdir(thumbnails_folder)
-
-    def __count_images(self):
-        return len(self.__get_all_image_names())
-
-    def __count_thumbnails(self):
-        return len(self.__get_all_thumbnail_names())
