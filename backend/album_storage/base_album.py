b267922152323e91a71ede783d60fab5128c5b55
Simen Holmestad <simehol@stud.ntnu.no>
2020-12-16 14:46:11 +0100

    Update abstract methods of base_album

--- backend/album_storage/base_album.py
+++ backend/album_storage/base_album.py
@@ -4,5 +4,30 @@ from abc import ABC, abstractmethod
 class BaseAlbum(ABC):
     """Base class for album-related storage of images"""
 
-    def __init__(self):
+    @abstractmethod
+    def get_album_description(self):
+        pass
+
+    @abstractmethod
+    def set_album_description(self, content):
+        pass
+
+    @abstractmethod
+    def get_relative_url_of_last_image(self):
+        pass
+
+    @abstractmethod
+    def get_relative_urls_of_all_images(self):
+        pass
+
+    @abstractmethod
+    def get_relative_urls_of_all_thumbnails(self):
+        pass
+
+    @abstractmethod
+    def try_capture_image_to_album(self, camera_module):
+        pass
+
+    @abstractmethod
+    def ensure_thumbnails_correct(self):
         pass
